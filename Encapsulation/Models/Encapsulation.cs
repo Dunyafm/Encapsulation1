ec5308bc98d460c2149b58200959d31b796fff1a
unknown <Dunyafm@code.edu.az>
2021-12-12 12:37:05 -0800

    Encapsulation

---
+++ Encapsulation/Models/Encapsulation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Encapsulation1.Models
+{
+    public class Encapsulation : Program
+    {
+
+        private string studentName;
+        private int studentAge;
+
+        public string Name
+        {
+
+            get
+            {
+                return studentName;
+            }
+
+            set
+            {
+                studentName = value;
+            }
+
+        }
+
+        public int Age
+        {
+
+            get
+            {
+                return studentAge;
+            }
+
+            set
+            {
+                studentAge = value;
+            }
+
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using Encapsulation1.Models;
+using System;
+
+namespace Encapsulation1
+{
+    public class Program
+    {
+
+    }
+    static public void Main()
+    {
+        {
+            var obj = new Encapsulation
+
+            string Name = "Leyla";
+
+            int Age = 21;
+            
+        }
+
+
+
+
+        
+        Console.WriteLine("Name: " );
+        Console.WriteLine("Age: ");
+    }
+}
+       
